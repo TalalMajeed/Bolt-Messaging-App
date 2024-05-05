5faf2d0ef1fbdc43e70f2b733b4f9824de8a49d4
TalalMajeed <m.talal.majeed@gmail.com>
2024-05-05 17:59:44 +0500

    V.1.0.0 - Initialized Project

---
+++ Frontend/src/components/App.jsx
@@ -0,0 +1,9 @@
+import "./App.scss";
+
+const App = () => {
+    return <div id="app">
+        <div>Coming Soon!</div>
+    </div>
+}
+
+export default App;
---
+++ README.md
@@ -0,0 +1,2 @@
+# Bolt Messaging App
+A Full Stack Instant Chat Application. Powered by JS Web Sockets!
